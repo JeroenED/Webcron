e7bf66541b2cb3d4892f386a2690be163f788ad6
Jeroen De Meerleer <me@jeroened.be>
2021-04-06 23:19:51 +0200

    Created some routes

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,12 +4,16 @@
 namespace JeroenED\Webcron\Controller;
 
 use JeroenED\Framework\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 class SecurityController extends Controller
 {
     public function loginAction(): Response
     {
+        if(isset($_SESSION['isAuthenticated']) && $_SESSION['isAuthenticated']) {
+            return new RedirectResponse($this->generateRoute('default'));
+        }
         return $this->render('security/login.html.twig');
     }
 }
