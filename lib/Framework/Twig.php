05e8ee6a8cc92734febf2f20d5d942b953ca00c7
Jeroen De Meerleer <me@jeroened.be>
2021-04-08 13:28:13 +0200

    Using date and interval filters

--- lib/Framework/Twig.php
+++ lib/Framework/Twig.php
@@ -6,6 +6,7 @@ namespace JeroenED\Framework;
 
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
+use Twig\TwigFilter;
 use Twig\TwigFunction;
 
 class Twig
@@ -19,6 +20,7 @@ class Twig
         $this->environment = new Environment($loader);
         $this->kernel = $kernel;
         $this->addFunctions();
+        $this->addFilters();
     }
 
     public function render(string $template, array $vars = []): string
@@ -32,6 +34,28 @@ class Twig
             return $this->kernel->getRouter()->getUrlForRoute($route);
         });
         $this->environment->addFunction($path);
+
     }
+
+    public function addFilters() {
+        $secondsToInterval = new TwigFilter('interval', function(int $time) {
+            $days = floor($time / (60 * 60 * 24));
+            $time -= $days * (60 * 60 * 24);
+
+            $hours = floor($time / (60 * 60));
+            $time -= $hours * (60 * 60);
+
+            $minutes = floor($time / 60);
+            $time -= $minutes * 60;
+
+            $seconds = floor($time);
+            $time -= $seconds;
+
+            return "{$days}d {$hours}h {$minutes}m {$seconds}s";
+        });
+        $this->environment->addFilter($secondsToInterval);
+    }
+
+
 
 }
