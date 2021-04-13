7842346220f724d754676a66c45d536e324516a9
Jeroen De Meerleer <me@jeroened.be>
2021-04-13 14:07:11 +0200

    NEW FEATURE: added http requests

--- src/Controller/JobController.php
+++ src/Controller/JobController.php
@@ -27,6 +27,21 @@ class JobController extends Controller
 
     public function addAction()
     {
-        return $this->render('job/add.html.twig');
+        if($this->getRequest()->getMethod() == 'GET') {
+            return $this->render('job/add.html.twig');
+        } elseif ($this->getRequest()->getMethod() == 'POST') {
+            $allValues = $this->getRequest()->request->all();
+            $jobRepo = new Job($this->getDbCon());
+            $joboutput = $jobRepo->addJob($allValues);
+            if($joboutput['success']) {
+                $this->addFlash('success', $joboutput['message']);
+                return new RedirectResponse($this->generateRoute('job_index'));
+            } else {
+                $this->addFlash('danger', $joboutput['message']);
+                return new RedirectResponse($this->generateRoute('job_add'));
+            }
+        } else {
+            return new Response('Not implemented yet', Response::HTTP_TOO_EARLY);
+        }
     }
 }
