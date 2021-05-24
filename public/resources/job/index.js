a2d09cfdd60c3863be383ca6e3bafcac8e0534de
Jeroen De Meerleer <me@jeroened.be>
2021-05-24 14:08:30 +0200

    Added job deletions

---
+++ public/resources/job/index.js
@@ -0,0 +1,21 @@
+$(function() {
+    initDeleteButtons();
+})
+
+function initDeleteButtons() {
+    $('.delete-btn').on('click', function() {
+        let me = $(this)
+        let href = me.data('href');
+        let confirmation = me.data('confirmation');
+
+        if(confirm(confirmation)) {
+            $.ajax({
+                url: href,
+                method: 'DELETE',
+                success: function(data) {
+                    window.location.href = data.return_path;
+                }
+            })
+        }
+    })
+}
