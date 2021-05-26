5ced8b46f091126cd235071b7ebf0ea6fbc5b5d3
Jeroen De Meerleer <me@jeroened.be>
2021-05-26 15:25:02 +0200

    Added reverse proxy support

--- lib/Framework/Kernel.php
+++ lib/Framework/Kernel.php
@@ -94,6 +94,7 @@ class Kernel
 
     private function parseRequest(): Request
     {
+        Request::setTrustedProxies(explode(',', $_ENV['TRUSTED_PROXIES']), Request::HEADER_X_FORWARDED_FOR | Request::HEADER_X_FORWARDED_HOST | Request::HEADER_X_FORWARDED_PORT | Request::HEADER_X_FORWARDED_PROTO);
         $request = Request::createFromGlobals();
         return $request;
     }
