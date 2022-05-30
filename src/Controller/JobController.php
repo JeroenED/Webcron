a0faaf941435345c15821b3e56bd37d914472f1b
Jeroen De Meerleer <me@jeroened.be>
2022-05-30 13:34:35 +0200

    BUGFIX: Better translations

--- src/Controller/JobController.php
+++ src/Controller/JobController.php
@@ -91,15 +91,15 @@ class JobController extends AbstractController
                 $return = [
                     'status' => 'deferred',
                     'success' => NULL,
-                    'title' => $translator->trans('job.runnow.deferred.title'),
-                    'message' => $translator->trans('job.runnow.deferred.message')
+                    'title' => $translator->trans('job.view.runnow.deferred.title'),
+                    'message' => $translator->trans('job.view.runnow.deferred.message')
                 ];
             } else {
                 $return = [
                     'status' => 'ran',
                     'success' => $runnowResult['success'],
-                    'title' => $runnowResult['success'] ? $translator->trans('job.runnow.ran.title.success') : $translator->trans('job.runnow.ran.title.failed'),
-                    'message' => $translator->trans('job.runnow.ran.message', [
+                    'title' => $runnowResult['success'] ? $translator->trans('job.view.runnow.ran.title.success') : $translator->trans('job.view.runnow.ran.title.failed'),
+                    'message' => $translator->trans('job.view.runnow.ran.message', [
                         '_runtime_' => number_format($runnowResult['runtime'], 3),
                         '_exitcode_' => $runnowResult['exitcode']
                     ]),
