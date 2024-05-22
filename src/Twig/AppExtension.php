4b3c4f1755553ccdaffa46f1f9334ecd89ea8f28
Jeroen De Meerleer <me@jeroened.be>
2024-05-22 11:24:51 +0200

    Update HSP equation source link in color conversion method
    The commit updates the source link for the HSP equation used in the color conversion method to a more accurate reference.

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -66,7 +66,7 @@ class AppExtension extends AbstractExtension
             $value = hexdec($value);
         }
 
-        // HSP (Highly Sensitive Poo) equation from http://alienryderflex.com/hsp.html
+        // HSP equation from http://alienryderflex.com/hsp.html
         $hsp = sqrt(
             0.299 * ($color[0] * $color[0]) +
             0.587 * ($color[1] * $color[1]) +
